70cb805cbd23dbcb8b4584d1669fb4e6303d541d
Wasd <81595311+WasdWasTaken@users.noreply.github.com>
2021-04-19 11:09:11 -0400

    credits: @CTK-WORRIOR

---
+++ handlers/command handler.js
@@ -0,0 +1,28 @@
+const { readdirSync } = require("fs");
+
+
+
+// Create a new Ascii table
+
+
+module.exports = (client) => {
+    // Read every commands subfolder
+    readdirSync("./commands/").forEach(dir => {
+        // Filter so we only have .js command files
+        const commands = readdirSync(`./commands/${dir}/`).filter(file => file.endsWith(".js"));
+    
+        // Loop over the commands, and add all of them to a collection
+        // If there's no name found, prevent it from returning an error,
+        // By using a cross in the table we made.
+        for (let file of commands) {
+            let pull = require(`../commands/${dir}/${file}`);
+    
+    
+    
+            // If there's an aliases key, read the aliases.
+            if (pull.aliases && Array.isArray(pull.aliases)) pull.aliases.forEach(alias => client.aliases.set(alias, pull.name));
+        }
+    });
+
+
+}
