f8ce056439828bfe23830344747a12d629abbafe
Wasd <81595311+WasdWasTaken@users.noreply.github.com>
2021-04-19 11:05:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const fs = require('fs')
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
